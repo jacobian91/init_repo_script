b7cc85ed20ba8629ef78bd9225fbcab631736b0b
Jacob Erickson <jacob.erickson@wibotic.com>
2022-10-07 01:02:21 -0700

    Change default gitignore, fix pylintrc generation

--- init.bat
+++ init.bat
@@ -2,8 +2,10 @@
 rem the parenthesis form a "block" which is loaded into memory so we can deal with deleting while running easier
 
 @REM Create Text Files
-echo .vscode > .gitignore
+echo off
+echo *cache* > .gitignore
 echo .local_gitignore > .local_gitignore
+echo .vscode >> .local_gitignore
 echo Hello World > README.md
 mkdir .vscode
 echo {^"code-runner.runInTerminal^": true} > ./.vscode/settings.json
@@ -15,8 +17,9 @@ git config core.excludesFile ./.local_gitignore
 @REM Setup Python
 poetry init -n
 poetry add pylint black mypy --dev
-pylint --generate-rcfile > .pylintrc
+poetry run pylint --generate-rcfile > .pylintrc
 
 @REM Delete this script
 del init.bat
+pause
 )
